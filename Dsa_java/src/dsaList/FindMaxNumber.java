da0b7b1a17baebb72efbb2fc9c558a2f13bc6de8
Prasad Shedage <prasadshedage1998@gmail.com>
2026-01-18 12:13:05 +0530

    find max number

---
+++ Dsa_java/src/dsaList/FindMaxNumber.java
@@ -0,0 +1,21 @@
+package dsaList;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.stream.Stream;
+
+public class FindMaxNumber {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> a=new LinkedList<Integer>();
+		a.add(12);
+		a.add(34);
+		a.add(2);
+		a.add(78);
+		Collections.sort(a);
+		for (Integer integer : a) {
+			System.out.println(integer);
+		}
+	}
+
+}
